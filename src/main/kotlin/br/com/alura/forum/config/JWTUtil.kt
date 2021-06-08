31a56cd2419f3571fc1617929d6839772ea6fad0
João Victor <jvc.martins12@gmail.com>
2021-06-07 22:15:31 -0300

    gerando tokens

--- src/main/kotlin/br/com/alura/forum/config/JWTUtil.kt
+++ src/main/kotlin/br/com/alura/forum/config/JWTUtil.kt
@@ -3,14 +3,16 @@ package br.com.alura.forum.config
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 import java.util.*
 
+@Component
 class JWTUtil {
 
-    private val expiration: Long = 60000
+    private val expiration : Long = 60000
 
     @Value("\${jwt.secret}")
-    private lateinit var secret: String
+    private lateinit var secret : String
 
     fun generateToken(username: String): String? {
         return Jwts.builder()
