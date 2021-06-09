b1e9205732467b909d767203b8b1b8c03708e6ac
João Victor <jvc.martins12@gmail.com>
2021-06-08 23:47:57 -0300

    gerando token da aplicação ainda sem validação

--- src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config
 
+import br.com.alura.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
