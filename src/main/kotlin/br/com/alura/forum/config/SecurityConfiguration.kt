b563a1ab6e6987f6698449a21f210d42993d056a
João Victor <jvc.martins12@gmail.com>
2021-06-08 22:48:29 -0300

    criando jwt login filter

--- src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
@@ -9,22 +9,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration(
-        private val userDetailsService: UserDetailsService
+        private val userDetailsService: UserDetailsService,
+        private val jwtUtil: JWTUtil
 )
     : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
         http?.
         authorizeRequests()?.
-        antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
+        //antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
+        antMatchers("/login")?.permitAll()?.
         anyRequest()?.
         authenticated()?.
-        and()?.
-        sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
+        and()
+        http?.addFilterBefore(JWTLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
         and()?.
         formLogin()?.disable()?.
         httpBasic()
