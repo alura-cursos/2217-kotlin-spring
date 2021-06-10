a8af875c6a4bc389d70fbf7a9efd6ec4dda75e82
João Victor <jvc.martins12@gmail.com>
2021-06-09 23:02:21 -0300

    pronto para testar autenticação

--- src/main/kotlin/br/com/alura/forum/config/JWTUtil.kt
+++ src/main/kotlin/br/com/alura/forum/config/JWTUtil.kt
@@ -3,7 +3,10 @@ package br.com.alura.forum.config
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
+import java.lang.IllegalArgumentException
 import java.util.*
 
 @Component
@@ -20,5 +23,19 @@ class JWTUtil {
                 .setExpiration(Date(System.currentTimeMillis() + expiration))
                 .signWith(SignatureAlgorithm.HS512, secret.toByteArray())
                 .compact()
+    }
+
+    fun isValid(jwt: String?): Boolean {
+        return try {
+            Jwts.parser().setSigningKey(secret.toByteArray()).parseClaimsJws(jwt)
+            true
+        } catch (e: IllegalArgumentException) {
+            false
+        }
+    }
+
+    fun getAuthentication(jwt: String?) : Authentication {
+        val username = Jwts.parser().setSigningKey(secret.toByteArray()).parseClaimsJws(jwt).body.subject
+        return UsernamePasswordAuthenticationToken(username, null, null)
     }
 }
--- src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
@@ -1,8 +1,10 @@
 package br.com.alura.forum.config
 
+import br.com.alura.forum.security.JWTAuthenticationFilter
 import br.com.alura.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -11,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
@@ -24,15 +27,13 @@ class SecurityConfiguration(
         http?.
         authorizeRequests()?.
         //antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
-        antMatchers("/login")?.permitAll()?.
+        antMatchers(HttpMethod.POST,"/login")?.permitAll()?.
         anyRequest()?.
         authenticated()?.
         and()
         http?.addFilterBefore(JWTLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
-        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
-        and()?.
-        formLogin()?.disable()?.
-        httpBasic()
+        http?.addFilterBefore(JWTAuthenticationFilter(jwtUtil = jwtUtil), OncePerRequestFilter::class.java)
+        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 
     @Bean
